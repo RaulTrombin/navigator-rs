8c3d11f01eaebb31d16e61c98c0ab046e2104ab4
RaulTrombin <RaulTrombin@users.noreply.github.com>
2024-11-24 18:37:10 +0000

    deploy: 91beb91cdf878c51dbb12fa8fa2e538c36fdb6e0

--- doc/type.impl/std/primitive.u16.js
+++
@@ -1,5 +0,0 @@
-(function() {var type_impls = {
-"embedded_hal":[],
-"nix":[],
-"serde":[]
-};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
